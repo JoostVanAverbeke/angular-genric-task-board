79a0765ece99b61135d107f9883f74a2d612fd11
Joost Van Averbeke <joost.van.averbeke@telenet.be>
2017-09-01 17:35:19 +0200

    Added accepts for dragula service for board of cards

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,6 +6,8 @@ import {BoardService} from "./board.service";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {Board} from "./board";
 import {BoardSwimlanes} from "./board.swimlanes";
+import {DragulaService} from "ng2-dragula";
+import {ModificationStatus} from "../modification/modification.status";
 
 @Component({
   selector: 'app-board',
@@ -17,8 +19,19 @@ export class BoardComponent implements OnInit {
   cards: Array<Card>;
   boardSwimlanes: BoardSwimlanes;
 
-  constructor(private boardService: BoardService, private route: ActivatedRoute) {
+  constructor(private boardService: BoardService,
+              private route: ActivatedRoute,
+              private dragulaService: DragulaService) {
     this.boardSwimlanes = new BoardSwimlanes();
+    dragulaService.setOptions('first-bag', {
+      accepts: function (el: HTMLElement, target: HTMLElement, source: HTMLElement, sibling: HTMLElement) {
+
+        let sourceStatus = <ModificationStatus> Number(source.id);
+        let targetStatus = <ModificationStatus> Number(target.id);
+        console.debug({el, target, source, sibling});
+        return sourceStatus == targetStatus; // elements can be dropped in any of the `containers` by default
+      }
+    })
   }
 
   ngOnInit(): void {
@@ -29,5 +42,23 @@ export class BoardComponent implements OnInit {
       .switchMap((params: ParamMap)  => this.boardService.getCardsOfBoard(+params.get('id')))
       .subscribe(cards => this.boardSwimlanes.setCardsInSwimlanes(cards));
   }
+
+  /*
+    Target Modification Status is represented in the Column
+    Source Modification Status is represented by the Row
+    Each cell value represents if it allowed to change to that Modification Status
+
+      Target ->ANL   ARVW   COD   CRVW   MRG   TST   DOC   FRVW   CMT
+
+      ANL       X     X
+      ARVW      X     X      X
+      COD             X      X     X
+      CRVW                   X     X     X
+      MRG                    X           X      X
+      TST                    X                  X    X
+      DOC                    X                       X      X
+      FRVW      X            X                              X     X
+      CMT                                                         X
+   */
 
 }
